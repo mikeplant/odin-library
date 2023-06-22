776464da97620fdd49ba1981beebafc55bbfd4e7
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-06-22 12:32:02 +0100

    Add input form for adding new books, add Book constructor

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, genre, pages, hasRead) {
+  this.title = title
+  this.author = author
+  this.genre = genre
+  this.pages = pages
+  this.hasRead = hasRead
+}
+
+function addBookToLibrary() {
+  
+}
