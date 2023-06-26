cff935a8ab5481ba28fdcb5751b814f35c635c3b
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-06-26 15:30:15 +0100

    Add remove and add book functions

--- js/script.js
+++ js/script.js
@@ -45,40 +45,44 @@ bookDisplay.addEventListener('click', (e) => {
   if (e.target.classList.contains('book-display')) return;
 
   if (e.target.type === 'button' || e.target.type === 'checkbox') { 
-    
+    if (e.target.classList.contains('remove-btn')) {
+      let parent = e.target.parentNode.parentNode;
+      let bookId = parseInt(parent.dataset.id);
+      removeBook(bookId);
+      parent.remove();
+    }
     return;
   }
 
   const bookEnd = !(e.target.classList.contains('book-end') || e.target.classList.contains('book-open')) ? e.target.parentNode : e.target;
-  const children = bookEnd.children;
+  const children = Array.from(bookEnd.children);
 
   bookEnd.classList.toggle('book-open');
   if (bookEnd.classList.contains('book-open')) {
-    Array.from(children).forEach(child => child.classList.remove('hidden'));
+    children.forEach(child => child.classList.remove('hidden'));
   } else {
-    Array.from(children).forEach(child => (child.classList.contains('hideable')) ? child.classList.add('hidden') : '');
+    children.forEach(child => (child.classList.contains('hideable')) ? child.classList.add('hidden') : '');
   }
 });
 
-//Dummy books
 
-let book = new Book('The Blade Itself','Joe Abercrombie','Fiction','515',true, 0);
-let book2 = new Book('Before They Are Hanged','Joe Abercrombie','Fiction','441',true, 1);
-let book3 = new Book('Last Argument of Kings','Joe Abercrombie','Fiction', '347', true, 2);
-let book4 = new Book('Half A King','Joe Abercrombie','Fiction','416',false, 3);
-let book5 = new Book('Good Omens', 'Terry Pratchett & Neil Gaiman', 'Fiction', '415', true, 4);
-let book6 = new Book('Animal Farm', 'George Orwell', 'Fiction', '122', true, 5);
-let book7 = new Book('Reamde','Neil Stephenson', 'Fiction', '1044', true, 6);
-myLibrary.push(book, book2, book3, book4, book5, book6, book7);
+function removeBook(removeId) {
+  myLibrary = myLibrary.filter(book => book.id !== removeId);
+}
+
+
 
 /////////////
 
 
 
-myLibrary.forEach(book => {
-  const bookEnd = createBookEnd(book.title, book.author, book.genre, book.pages, book.hasRead, book.id);
-  bookDisplay.appendChild(bookEnd);
-});
+function displayBooks() {
+  bookDisplay.innerHTML = '';
+  myLibrary.forEach(book => {
+    const bookEnd = createBookEnd(book.title, book.author, book.genre, book.pages, book.hasRead, book.id);
+    bookDisplay.appendChild(bookEnd);
+  });
+}
 
 function createBookEnd(title, author, genre, pages, hasRead, id) {
   const div = document.createElement('div');
@@ -95,9 +99,49 @@ function createBookEnd(title, author, genre, pages, hasRead, id) {
       <input type="checkbox" name="has-read-edit" class="has-read" ${(hasRead) ? "checked" : ""}>
     </label>
     <div class="book-open-btns hidden hideable">
-      <button type="button" class="btn">Remove</button>
+      <button type="button" class="btn remove-btn">Remove</button>
       <button type="button" class="btn">Close</button>
     </div>
   `
   return div;
 }
+
+const addBookForm = document.querySelector('.add-book-form');
+let bookIdNumbers = [0,1,2,3,4,5,6]
+
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let bookArgs = [
+    document.querySelector('#title').value,
+    document.querySelector('#author').value,
+    document.querySelector('#genre').value,
+    document.querySelector('#pages').value,
+    document.querySelector('#has-read').checked,
+    assignBookId()
+  ]
+  let book = new Book(...bookArgs);
+  myLibrary.push(book);
+  displayBooks();
+  addBookForm.reset();
+});
+
+function assignBookId() {
+  let newId = parseInt(bookIdNumbers.slice(-1)) + 1;
+  bookIdNumbers.push(newId);
+  return newId;
+}
+
+
+
+//Dummy books
+
+let book = new Book('The Blade Itself','Joe Abercrombie','Fiction','515',true, 0);
+let book2 = new Book('Before They Are Hanged','Joe Abercrombie','Fiction','441',true, 1);
+let book3 = new Book('Last Argument of Kings','Joe Abercrombie','Fiction', '347', true, 2);
+let book4 = new Book('Half A King','Joe Abercrombie','Fiction','416',false, 3);
+let book5 = new Book('Good Omens', 'Terry Pratchett & Neil Gaiman', 'Fiction', '415', true, 4);
+let book6 = new Book('Animal Farm', 'George Orwell', 'Fiction', '122', true, 5);
+let book7 = new Book('Reamde','Neil Stephenson', 'Fiction', '1044', true, 6);
+myLibrary.push(book, book2, book3, book4, book5, book6, book7);
+
+displayBooks();
